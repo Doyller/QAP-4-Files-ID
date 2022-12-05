b0076e1f7f9a5812549aa2419b655e688efc505a
Doyller <117834332+Doyller@users.noreply.github.com>
2022-12-04 20:37:36 -0330

    Add files via upload
    QAP pt. 1

--- main.py
+++ main.py
@@ -1,40 +1,122 @@
-# matplotlib claim graphing
+# QAP 4, Isaac Doyle SD7
+# Data files for One stop insurance company.
 
-import matplotlib.pyplot as plt
+import datetime
+from datetime import timedelta
+import FormatValues as FV
 
+# Open OSICDef.dat file and read values into variables
+
+f = open("OSICdef.dat", "r")
+POLICY_NUM = int(f.readline())
+BASE_RATE = float(f.readline())
+ADD_DISC_RATE = float(f.readline())
+ADD_LIB_RATE = float(f.readline())
+GLASS_COV_RATE = float(f.readline())
+LOAN_CAR_RATE = float(f.readline())
+HST_RATE = float(f.readline())
+PROS_FEE = float(f.readline())
+f.close()
+
+#Needed inputs for program
 while True:
-    CJan = float(input("Enter the dollar value of claims in January: "))
-    CFeb = float(input("Enter the dollar value of claims in February: "))
-    CMar = float(input("Enter the dollar value of claims in March: "))
-    CApr = float(input("Enter the dollar value of claims in April: "))
-    CMay = float(input("Enter the dollar value of claims in May: "))
-    CJune = float(input("Enter the dollar value of claims in June: "))
-    CJuly = float(input("Enter the dollar value of claims in July: "))
-    CAug = float(input("Enter the dollar value of claims in August: "))
-    CSept = float(input("Enter the dollar value of claims in September: "))
-    COct = float(input("Enter the dollar value of claims in October: "))
-    CNov = float(input("Enter the dollar value of claims in November: "))
-    CDec = float(input("Enter the dollar value of claims in December: "))
-    Jan = "Jan"
-    Feb = "Feb"
-    Mar = "Mar"
-    Apr = "Apr"
-    May = "May"
-    June = "June"
-    July = "July"
-    Aug = "Aug"
-    Sept = "Sept"
-    Oct = "Oct"
-    Nov = "Nov"
-    Dec = "Dec"
+    CxNameFirst = input("Enter the cx first name: ").title()
+    CxNameLast = input("Enter the cx last name: ").title()
+    CxAddress = input("Enter the cx address: ").title()
+    City = input("Enter the cx city: ").title()
+    Prov = input("Enter the cx province: ").title()
+    Postal = input("Enter the cx postal code (X1X 1X1):").upper()
+    PhoneNum = input("Enter the cx phone number (xxx-xxx-xxxx): ")
+    NumCars = int(input("Enter the number of cars being insured: "))
+    ExtraLiab = input("Enter whether the cx wants extra liability (Y or N): ").upper()
+    OptLoanCar = input("Enter whether optional loaner car (Y or N): ").upper()
+    OptGlassCov = input("Enter whether the cx would like additional glass coverage (Y or N): ").upper()
+    PayOption = input("Enter whether cx will pay in full or monthly (F or M): ").upper()
+    PolicyStartDate = datetime.datetime.now()
+    PolicyStartDateDsp = PolicyStartDate.strftime("%Y-%m-%d")
 
-    x = [Jan, Feb, Mar, Apr, May, June, July, Aug, Sept, Oct, Nov, Dec]
-    y = [CJan, CFeb, CMar, CApr, CMay, CJune, CJuly, CAug, CSept, COct, CNov, CDec]
+#Calculations needed for program
+    if NumCars == 1:
+        BaseCost = BASE_RATE
+    else:
+        BaseCost = BASE_RATE * NumCars - (((NumCars - 1) * BASE_RATE) * ADD_DISC_RATE)
 
-    plt.title('Claim amounts month over month')
-    plt.scatter(x, y,   color='darkblue', marker='x', label="Monthly claim")
-    plt.xlabel("Month")
-    plt.ylabel("Dollar Value")
-    plt.grid(True)
-    plt.legend()
-    plt.show()
+    if ExtraLiab == "Y":
+        ExtraLibCost = ADD_LIB_RATE * NumCars
+    elif ExtraLiab == "N":
+        ExtraLibCost = 0
+    else:
+        break
+
+    if OptGlassCov == "Y":
+        OptGlassCost = GLASS_COV_RATE * NumCars
+    elif OptGlassCov == "N":
+        OptGlassCost = 0
+    else:
+        break
+
+    if OptLoanCar == "Y":
+        OptLoanCost = LOAN_CAR_RATE * NumCars
+    elif OptLoanCar == "N":
+        OptLoanCost = 0
+    else:
+        break
+
+    ExtraCost = ExtraLibCost + OptLoanCost + OptGlassCost
+    TotInsPrem = ExtraCost + BaseCost
+    HST = TotInsPrem * HST_RATE
+    TotFullCost = TotInsPrem + HST
+
+    if PayOption == "M":
+        MonPayment = (TotFullCost + PROS_FEE) / 8
+    else:
+        MonPayment = 0
+
+    if PolicyStartDateDsp > "25":
+        PolicyStartDateDsp = ((PolicyStartDate.replace(day=1) + timedelta(days=32)).replace(day=1))
+
+
+    print("-------------------------------------------")
+    print("One Stop Insurance Company Invoice")
+    print()
+    print(f"{POLICY_NUM}-{CxNameFirst[0]}{CxNameLast[0]} ")
+    print(f"{CxAddress:<20s}, {City:<10s}, {Prov:<10s}")
+    print(f"{Postal:<6s},     {PhoneNum:<10s}")
+    print(f"Policy start Date:               {(PolicyStartDateDsp)}")
+    print("-------------------------------------------")
+    print(f"Number of vehicles on policy:          {NumCars}")
+    print()
+    print(f"Cost of Extra liability:           {FV.FDollar2(ExtraLibCost)}")
+    print(f"Cost of Loaner car option:         {FV.FDollar2(OptLoanCost)}")
+    print(f"Cost of Optional glass coverage:   {FV.FDollar2(OptGlassCost)}")
+    print("                                   ---------")
+    print(f"Total Cost of extra coverage:      {FV.FDollar2(ExtraCost)}")
+    print(f"Cost of base policy:               {FV.FDollar2(BaseCost)}")
+    print(f"Total premium cost:                {FV.FDollar2(TotInsPrem)}")
+    print(f"HST:                               {FV.FDollar2(HST)}")
+    print("                                   ---------")
+    print(f"Total cost plus HST (@15%)         {FV.FDollar2(TotFullCost)}")
+    if PayOption == "M":
+        print(f"Monthly payment                    {FV.FDollar2(MonPayment)}")
+    else:
+        break
+
+    f = open("Policies.dat", "a")
+    f.write("{}, ".format(int(POLICY_NUM)))
+    f.write("{}, ".format(CxNameFirst))
+    f.write("{}, ".format(CxNameLast))
+    f.write("{}, ".format(CxAddress))
+    f.write("{}, ".format(City))
+    f.write("{}, ".format(Prov))
+    f.write("{}, ".format(Postal))
+    f.write("{}, ".format(PhoneNum))
+    f.write("{}, ".format(NumCars))
+    f.write("{}, ".format(ExtraLiab))
+    f.write("{}, ".format(OptLoanCar))
+    f.write("{}, ".format(OptGlassCov))
+    f.write("{}, ".format(PayOption))
+    f.write("{}\n ".format(FV.FDollar2(TotFullCost)))
+    f.close()
+    print()
+    print("Policy information processed and saved.")
+    POLICY_NUM += 1
